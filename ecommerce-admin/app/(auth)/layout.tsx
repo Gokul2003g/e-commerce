9fcd7a37cc224ae3a6b8725190e8901f1dc5e952
Gokul2003g <75162131+Gokul2003g@users.noreply.github.com>
2024-04-02 19:02:09 +0530

    app done

--- ecommerce-admin/app/(auth)/layout.tsx
+++ ecommerce-admin/app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex items-center justify-center h-screen">{children}</div>
   );
