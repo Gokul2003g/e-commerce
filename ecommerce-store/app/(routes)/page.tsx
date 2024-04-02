15df3a122942c7a6333ecd9c27ff412e0a337b97
Gokul2003g <75162131+Gokul2003g@users.noreply.github.com>
2024-04-02 22:15:53 +0530

    billboard

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("d416bd2c-78cd-479f-8154-7a3505c23096");
 
   return (
     <Container>
